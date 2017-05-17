6cb47bf2eebdc9d6e55009869749f7a0ea804bb2
davy10357905 <davy0718@gmail.com>
2017-05-18 01:07:47 +0800

    use psql

--- ion_camp/settings.py
+++ ion_camp/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'ion_camp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ion_camp',
+        'USER': 'ion_camp_admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
